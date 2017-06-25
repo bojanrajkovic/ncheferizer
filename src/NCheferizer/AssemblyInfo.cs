a37655901753c343121a8e6ae272f6923ee46fb3
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-06-24 23:42:53 -0400

    NCheferizer: Add CI and versioning. (#1)
    Add CI info and set up versioning like other projects.

---
+++ src/NCheferizer/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using System.Reflection;
+
+[assembly: InternalsVisibleTo("NCheferizer.Tests")]
+
+[assembly: AssemblyCompany("Code, Rinse, Repeat")]
+
+[assembly: AssemblyVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor
+)]
+
+[assembly: AssemblyFileVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch
+)]
+
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch + "-" +
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit
+)]
